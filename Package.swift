1230a407a148ad5350d09e6bbdd1541582a55b67
Christian Treffs <ctreffs@gmail.com>
2019-08-30 11:42:24 +0200

    Remove CImGUI library product

--- Package.swift
+++ Package.swift
@@ -7,8 +7,6 @@ let package = Package(
     name: "ImGUI",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "CImGUI",
-                 targets: ["CImGUI"]),
         .library(
             name: "ImGUI",
             targets: ["ImGUI"])

837d36d7b358407af3a63a72bc57475bd22ffd5a
Christian Treffs <ctreffs@gmail.com>
2019-10-24 16:19:19 +0200

    Refactor structure

--- Package.swift
+++ Package.swift
@@ -1,36 +1,22 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 var package = Package(
     name: "ImGUI",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ImGUI",
-            type: .static,
-            targets: ["ImGUI"]),
-        .library(name: "CImGUI",
-                 targets: ["CImGUI"])
+        .library(name: "ImGUI", targets: ["ImGUI"]),
+        .library(name: "CImGUI", targets: ["CImGUI"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ImGUI",
-            dependencies: ["CImGUI"]),
-        .testTarget(
-            name: "ImGUITests",
-            dependencies: ["ImGUI"]),
-        .target(name: "CImGUI",
-                path: "Sources/CImGUI2",
-                cxxSettings: [.define("CIMGUI_DEFINE_ENUMS_AND_STRUCTS")]),
-        .target(name: "Generator")
+        .target(name: "ImGUI", dependencies: ["CImGUI"]),
+        .target(name: "CImGUI", cxxSettings: [.define("CIMGUI_DEFINE_ENUMS_AND_STRUCTS")]),
+        .target(name: "Generator"),
+        .testTarget(name: "ImGUITests", dependencies: ["ImGUI"])
     ],
     cxxLanguageStandard: .cxx11
 )

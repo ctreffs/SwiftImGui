d8debc82cbd5b8e5fb49d84b1fdf60deae2c39c9
Junhao Wang <junhaowanggg@gmail.com>
2022-03-16 04:48:44 -0700

    Update ImGui 1.86-docking (#8)
    * Update ImGui 1.86-docking
    
    * Update CI
    
    * Re-enable docking
    
    Co-authored-by: Christian Treffs <ctreffs@gmail.com>

--- Sources/Demos/Metal/ViewController.swift
+++ Sources/Demos/Metal/ViewController.swift
@@ -11,64 +11,65 @@ import MetalKit
 
 @available(OSX 10.11, *)
 final class ViewController: NSViewController {
-
+    
     let mtkView = MTKView()
     lazy var renderer = Renderer(mtkView)
-
+    
     override func loadView() {
         mtkView.wantsLayer = true
         mtkView.layer!.backgroundColor = .black
         self.view = mtkView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         mtkView.device = MTLCreateSystemDefaultDevice()
         mtkView.delegate = self.renderer
-
+        
         renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.bounds.size)
-
+        
         // Add a tracking area in order to receive mouse events whenever the mouse is within the bounds of our view
         let trackingArea: NSTrackingArea = NSTrackingArea(rect: .zero,
                                                           options: [.mouseMoved, .inVisibleRect, .activeAlways],
                                                           owner: self,
                                                           userInfo: nil)
         view.addTrackingArea(trackingArea)
-
+        
         // If we want to receive key events, we either need to be in the responder chain of the key view,
         // or else we can install a local monitor. The consequence of this heavy-handed approach is that
         // we receive events for all controls, not just Dear ImGui widgets. If we had native controls in our
         // window, we'd want to be much more careful than just ingesting the complete event stream, though we
         // do make an effort to be good citizens by passing along events when Dear ImGui doesn't want to capture.
-        NSEvent.addLocalMonitorForEvents(matching: .any) { [unowned self](event) -> NSEvent? in
-            let wantsCapture: Bool = ImGui_ImplOSX_HandleEvent(event, self.view)
-            if event.type == .keyDown && wantsCapture {
-                return nil
-            } else {
-                return event
-            }
+        let eventMask: NSEvent.EventTypeMask = [.keyDown, .keyUp, .flagsChanged]
+        NSEvent.addLocalMonitorForEvents(matching: eventMask) { [unowned self](event) -> NSEvent? in
+            ImGui_ImplOSX_HandleEvent(event, self.view)
+            return event
         }
-
-        ImGui_ImplOSX_Init()
+        
+        ImGui_ImplOSX_Init(self.view)
     }
-
+    
+    deinit {
+        ImGui_ImplOSX_Shutdown()
+    }
+    
     override func mouseMoved(with event: NSEvent) {
         ImGui_ImplOSX_HandleEvent(event, self.view)
     }
-
+    
     override func mouseDown(with event: NSEvent) {
         ImGui_ImplOSX_HandleEvent(event, self.view)
     }
-
+    
     override func mouseUp(with event: NSEvent) {
         ImGui_ImplOSX_HandleEvent(event, self.view)
     }
-
+    
     override func mouseDragged(with event: NSEvent) {
         ImGui_ImplOSX_HandleEvent(event, self.view)
     }
-
+    
     override func scrollWheel(with event: NSEvent) {
         ImGui_ImplOSX_HandleEvent(event, self.view)
     }
--- docs/HOWTO.md
+++ docs/HOWTO.md
@@ -1,18 +1,37 @@
-# Wrap cimgui
+# How To Wrap cimgui
 
-For the time beeing it is not possible to wrap cimgui out of the box fully automated.
+For the time being it is not possible to wrap cimgui out of the box. This process is not fully automated.
 Some manual steps are stil neccessary.
 This file serves more as a reminder to me, so don't expect it to be complete.
 
+### Notes
+- It is not recommended to manually edit these files. They are auto generated after `make update`.
+  - `3rdparty/cimgui` (git submodule)
+  - `Sources/CImGui`
+  - `Sources/ImGui`
+  - `Sources/AutoWrapper/Assets/definitions.json`
+- You should update these files if you see new errors after `make update`.
+  - `Sources/AutoWrapper` (excepts `definitions.json`)
+  - `patch_fix_ifdefs.diff`
+
 ### Steps
 
-0. Be sure to clone with clone recursive: `git clone --recursive git@github.com:ctreffs/SwiftImGui.git`
-1. In submodule 3rdparty/cimgui
-	0. `git submodule update --init --recursive` 
-	1. Update to latest master from <https://github.com/cimgui/cimgui>
-2. In repo make new feature branch
-3. Commit updated submodule pin
-4. Run `make update`
-5. Build, fix and run
-6. Update README.md
-7. Commit, push, tag, release 
+1. Fork and clone! Be sure to clone SwiftImGui recursively by `git clone --recursive ...`
+2. Before start, try running `make lint`, `make setupEnv`, and `make build-release`
+    - If `brew install luajit` fails, try `brew install luajit --HEAD` ([neovim #13529](https://github.com/neovim/neovim/issues/13529))
+3. Make new feature branch in SwiftImGui. Example: `git checkout -b update-1.82`
+4. Update submodule cimgui
+    - In `3rdparty/cimgui` run `git submodule update --init --recursive`
+    - Make sure it is up to the latest commit in [master branch](https://github.com/cimgui/cimgui)
+    - Make sure the imgui version inside 3rdparty/cimgui is up to date as well
+    - You should have a clean workplace after these steps 🤔
+5. Commit updated submodule changes
+6. Run `make update` and if you see errors, please try to fix them
+    - For patch fix issues, update `patch_fix_ifdefs.diff` to match the new differences
+    - For unresolved identifiers, you can update `Exceptions.swift`
+    - Re-run `make update` until it does not show errors
+    - Otherwise, open an issue and pull request for help
+7. Build and run the demos 🍻
+8. Update the provided backend files if there are major changes in the [original backend files](https://github.com/ocornut/imgui/tree/master/backends)
+9. Update `README.md` and this file
+10. Commit, push, tag, release 🎉
